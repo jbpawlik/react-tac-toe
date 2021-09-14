feb4b743e64e50d17fdd8c977e5eec10165a0462
James Benjamin Pawlik <james.benjamin.pawlik@gmail.com>
2021-09-14 15:08:28 -0700

    create index.js files

---
+++ src/state/ducks/game/index.js
@@ -0,0 +1,12 @@
+import reducer from './reducers';
+import * as gameOperations from './operations';
+import * as gameSelectors from './selectors';
+import * as gameTypes from './types';
+
+export {
+  gameOperations,
+  gameSelectors,
+  gameTypes
+};
+
+export default reducer;
