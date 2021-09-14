b60bded5cc997969fc45f2b20dac81517a7aaaeb
James Benjamin Pawlik <james.benjamin.pawlik@gmail.com>
2021-09-14 15:22:51 -0700

    update index.js and app.jsx
    Co-authored-by: bordonj <bordonj@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react';
+import { configureStore } from './state/store.js';
 import App from './views/App.jsx';
 
+const initialState = {};
+ const store = configureStore(initialState);
+
 render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('app')
 );
--- src/views/App.jsx
+++ src/views/App.jsx
@@ -1,9 +1,43 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux'
+import { gameOperations } from '../state/ducks/game';
 
-const App = () => {
+
+const App = ({ player, playTurn}) => {
+  const handleClick = () => {
+    playTurn(player, 0, 1);
+  };
+
   return (
+    <div>
       <h2>Hello Tic Tac Toe!</h2>
+      <p>
+        Current Player is: {player}
+      </p>
+      <p>
+        <button onClick={handleClick}>Make a move</button>
+      </p>
+    </div>
   );
 };
 
-export default App;
+const {number, func} = PropTypes;
+App.propTypes = {
+  player: number.isRequired,
+  playTurn: func.isRequired
+};
+
+const mapStateToProps = state => {
+const { gameState } = state;
+  return {
+    player: gameState.player,
+    winner: gameState.winner
+  }
+}
+
+const mapDispatchToProps = {
+  playTurn: gameOperations.playTurn
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
