b448f6dbd05a48305d0445549ec2f6aa579ea22d
James Benjamin Pawlik <james.benjamin.pawlik@gmail.com>
2021-09-14 14:52:30 -0700

    add operations
    Co-authored-by: bordonj <bordonj@users.noreply.github.com>

---
+++ src/state/ducks/game/operations.js
@@ -0,0 +1,36 @@
+import { newGame, gameover, switchPlayer, winnder, movePlayer } from "./actions";
+import { isWinner, isDraw } from '../../utils/game';
+
+const checkWinner = (board, player) => (dispatch) => {
+  if (isWinner(board, player)) {
+    dispatch(winner(player));
+    dispatch(gameover());
+  } else if (isDraw(board)) {
+    dispatch(winner(0));
+    dispatch(gameover());
+  }
+};
+
+const playTurn = (player, row, col) => (dispatch) => {
+  let nextPlayer;
+
+  switch (player) {
+    case 1:
+      nextPlayer = 2;
+      break;
+    case 2:
+      nextPlayer = 1;
+      break;
+    default:
+      break;
+  }
+
+  dispatch(movePlayer(player, row, col));
+  dispatch(switchPlayer(nextPlayer));
+}
+
+export {
+  newGame,
+  checkWinner,
+  playTurn
+};
