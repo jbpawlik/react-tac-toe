feaef7c4b46645e1c4e374394959ba3193fbd59d
James Benjamin Pawlik <james.benjamin.pawlik@gmail.com>
2021-09-15 09:38:52 -0700

    add Board.js and Square.js
    Co-authored-by: bordonj <bordonj@users.noreply.github.com>

--- src/views/components/Square.jsx
+++ src/views/components/Square.jsx
@@ -1,39 +1,32 @@
-import React from 'react';
+import React from "react";
 import PropTypes from 'prop-types';
-
-import { withStyles } from 'material-ui/styles';
+import { withStyles } from "material-ui/styles";
 import Icon from 'material-ui/Icon';
 
-const styles = (theme) => ({
+const styles = theme => ({
   icon: {
-    fontSize: '2em' // double the size of the square's font size
+    fontSize: '2em'
   }
 });
 
-const playerIcon = (player) => {
-  // these are icon names from https://material.io/icons/
-  // you can pick whichever you want, but I chose two that 
-  // closely resembles X and O
+const playerIcon = player => {
   switch (player) {
     case 1:
-      return 'clear'; // X
+      return 'clear';
     case 2:
-      return 'panorama_fish_eye'; // O
+      return 'radio_button_unchecked';
     default:
       return '';
   }
 };
 
-// the Square is just a square on the game board, it will have nothing, an X or an O depending 
-// on which player has marked the square
 const Square = ({ classes, player }) => {
-  // 'player' is the player, or none, that marked this Square
   return (
     <Icon className={classes.icon}>{playerIcon(player)}</Icon>
   );
 };
 
-const { object, number } = PropTypes;
+const {object, number} = PropTypes;
 
 Square.propTypes = {
   classes: object.isRequired,
@@ -41,3 +34,4 @@ Square.propTypes = {
 };
 
 export default withStyles(styles)(Square);
+
